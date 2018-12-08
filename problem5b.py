4c35b739bf997d985bc7810cbba87093cc0d306f
Micah Workman <nidoprince@gmail.com>
2018-12-08 15:39:47 -0700

    Finished day 5

--- problem5b.py
+++ problem5b.py
@@ -1,17 +1,21 @@
-polymer = open("inputDay5.txt","r")
-polymer = [x for x in polymer][0]
+oGpolymer = open("inputDay5.txt","r")
+oGpolymer = [x for x in oGpolymer][0]
+
 def react(a,b):
     return a!=b and a.upper() == b.upper()
 
-reactedPolymer = ""
-
-for element in polymer:
-    if(len(reactedPolymer) == 0):
-        reactedPolymer += element
-    else:
-        if(react(element,reactedPolymer[-1])):
-            reactedPolymer = reactedPolymer[:-1]
+def findPostReactionLength(polymer):
+    reactedPolymer = ""
+    for element in polymer:
+        if(len(reactedPolymer) == 0):
+            reactedPolymer += element
         else:
-            reactedPolymer+=element
+            if(react(element,reactedPolymer[-1])):
+                reactedPolymer = reactedPolymer[:-1]
+            else:
+                reactedPolymer+=element
+    return len(reactedPolymer)
 
-print(len(reactedPolymer))
+removals = ["aA","bB","cC","dD","eE","fF","gG","hH","iI","jJ","kK","lL","mM","nN","oO","pP","qQ","rR","sS","tT","uU","vV","wW","xX","yY","zZ"]
+polymerLengths = [findPostReactionLength(list(filter(lambda c:c not in x,oGpolymer))) for x in removals]
+print(sorted(polymerLengths)[0])
